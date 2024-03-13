9b5d7756e57de0d418efb37a5561e4e4501f2c38
Karry_Pham <85409706+hieuliempham@users.noreply.github.com>
2024-03-13 10:36:11 +0700

    Add project files.

---
+++ BasicMath/BasicMaths.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicMath
+{
+    public class BasicMaths
+    {
+      
+       public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            // To trace error while testing,  writing + operator instead of * operator.
+            return num1 * num2;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BasicMathTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicMath;
+
+namespace BasicMathTest
+{
+ 
+    [TestClass]
+    public class UnitTest1
+    {
+ 
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+
+        }
+
+        public void Test_Liem()
+        {
+            BasicMaths bm = new BasicMaths();
+            double a = bm.Add(10, 10); // 20
+            double b = bm.Substract(10, 5); //5
+            double c = bm.Multiply(10, 10); // 100
+            double res = a + b + c;
+            Assert.AreEqual(res, 125);
+
+        }
+    }
+}
